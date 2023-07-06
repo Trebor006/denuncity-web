8bc3afd62ee27d8992371332882e8970f6fafa66
Robert Cabrera <clrobert.06@gmail.com>
2023-07-05 23:34:02 -0400

    feat(master): Departamentos actualizados

--- src/pages/Departamentos/Departamento.tsx
+++ src/pages/Departamentos/Departamento.tsx
@@ -1,11 +1,35 @@
-import React, {useState} from "react";
-import axios from "axios";
-import {toast, ToastContainer} from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useParams, useLocation } from 'react-router-dom';
 
 const Departamento = () => {
-    const [nombre, setNombre] = useState("");
-    const [descripcion, setDescripcion] = useState("");
+    const { id } = useParams<{ id: string }>();
+    const location = useLocation();
+    const [nombre, setNombre] = useState('');
+    const [descripcion, setDescripcion] = useState('');
+    const [isEditing, setIsEditing] = useState(false);
+
+    useEffect(() => {
+        const cargarDepartamento = async () => {
+            if (id) {
+                try {
+                    const response = await axios.get(`http://localhost:3001/departamentos/buscar?id=${id}`);
+                    const departamento = response.data;
+                    setNombre(departamento.nombre);
+                    setDescripcion(departamento.descripcion);
+
+                } catch (error) {
+                    console.error('Error al cargar el departamento:', error);
+                }
+            }
+        };
+
+        setIsEditing(location.pathname.includes('editar') || location.pathname.includes('registrar'));
+
+        cargarDepartamento();
+    }, [id, location.pathname]);
 
     const handleNombreChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setNombre(event.target.value);
@@ -16,8 +40,8 @@ const Departamento = () => {
     };
 
     const handleGuardarClick = async () => {
-        if (nombre === "" || descripcion === "") {
-            toast.error("Por favor, complete todos los campos");
+        if (nombre === '' || descripcion === '') {
+            toast.error('Por favor, complete todos los campos');
             return;
         }
 
@@ -27,21 +51,21 @@ const Departamento = () => {
                 descripcion,
             };
 
-            // Enviar los datos al servidor
-            const response = await axios.post(
-                "http://localhost:3001/departamentos/registrar",
-                departamentoData
-            );
+            if (isEditing && id) {
+                const response = await axios.put(`http://localhost:3001/departamentos/${id}`, departamentoData);
+                toast.success('Área de trabajo actualizada exitosamente');
+                console.log('Respuesta del servidor:', response.data);
+            } else {
+                const response = await axios.post('http://localhost:3001/departamentos/registrar', departamentoData);
+                toast.success('Área de trabajo registrada exitosamente');
+                console.log('Respuesta del servidor:', response.data);
+            }
 
-            toast.success("Área de trabajo registrado exitosamente");
-            console.log("Respuesta del servidor:", response.data);
-
-            // Restablecer los valores del formulario
-            setNombre("");
-            setDescripcion("");
+            setNombre('');
+            setDescripcion('');
         } catch (error) {
-            toast.error("Error al guardar el área de trabajo");
-            console.error("Error al guardar el área de trabajo:", error);
+            toast.error('Error al guardar el área de trabajo');
+            console.error('Error al guardar el área de trabajo:', error);
         }
     };
 
@@ -58,6 +82,7 @@ const Departamento = () => {
                     value={nombre}
                     onChange={handleNombreChange}
                     className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
+                    disabled={!isEditing}
                 />
             </div>
             <div className="mb-4">
@@ -69,22 +94,26 @@ const Departamento = () => {
                     value={descripcion}
                     onChange={handleDescripcionChange}
                     className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
+                    disabled={!isEditing}
                 />
             </div>
             <div className="flex justify-end gap-4.5">
-                <a href="/departaments"
-                   className="flex justify-center rounded border border-stroke py-2 px-6 font-medium text-black hover:shadow-1 dark:border-strokedark dark:text-white"
+                <a
+                    href="/departaments"
+                    className="flex justify-center rounded border border-stroke py-2 px-6 font-medium text-black hover:shadow-1 dark:border-strokedark dark:text-white"
                 >
                     Cancelar
                 </a>
-                <button
-                    onClick={handleGuardarClick}
-                    className="inline-flex items-center justify-center rounded-md bg-meta-3 py-4 px-10 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10"
-                >
-                    Registrar Área
-                </button>
+                {isEditing && (
+                    <button
+                        onClick={handleGuardarClick}
+                        className="inline-flex items-center justify-center rounded-md bg-meta-3 py-4 px-10 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10"
+                    >
+                     Guardar
+                    </button>
+                )}
             </div>
-            <ToastContainer/>
+            <ToastContainer />
         </div>
     );
 };
